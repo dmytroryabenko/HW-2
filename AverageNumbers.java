e3e5c3ba4739cbfa6b6ba79e2608717aef3a17dc
dmytroryabenko <116278537+dmytroryabenko@users.noreply.github.com>
2022-10-30 17:08:17 +0200

    Update AverageNumbers.java

--- AverageNumbers.java
+++ AverageNumbers.java
@@ -4,15 +4,15 @@ public class AverageNumbers {
 
     public static void main(String[] args) {
 
-        int[] array = {10, 20, 35, 15};
+        double[] array = {10, 20, 35, 15};
 
-        int sum = 0;
+        double sum = 0;
 
-        for (int i : array) {
+        for (double i : array) {
 
             sum += i;
         }
-        int avg = sum / array.length;
+        double avg = sum / array.length;
 
         System.out.println(avg);
     }
