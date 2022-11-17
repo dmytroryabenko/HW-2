c69c703542674e7dd5961d70f4eac05d51e75bd7
dmytroryabenko <116278537+dmytroryabenko@users.noreply.github.com>
2022-11-17 22:21:49 +0200

    Update ReverseOrder.java

--- ReverseOrder.java
+++ ReverseOrder.java
@@ -1,13 +1,10 @@
 package Array1;
-
 import java.util.Arrays;
-
 public class ReverseOrder {
 
     public static void main(String[] args) {
 
         int[] num = {2, 3, 1, 7, 11,};
-
         for (int i = 0; i < num.length - 1; i++) {
             for (int j = 0; j < num.length - 1; j++) {
                 if (num[j] < num[j + 1]) {
