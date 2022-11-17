65ce2998afd782436c2e08e4c6814c916c6250a0
dmytroryabenko <116278537+dmytroryabenko@users.noreply.github.com>
2022-11-17 22:20:37 +0200

    Update ReturnTheSum.java

--- ReturnTheSum.java
+++ ReturnTheSum.java
@@ -1,17 +1,12 @@
 package Array1;
-
 public class ReturnTheSum {
 
     public static void main(String[] args) {
 
         int[] arr = {56, 87, 6, -9, 98, -876, 3, -34, 22};
-
         int sum = 0;
-
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] > 0) {
-
                 sum += arr[i];
             }
         }
