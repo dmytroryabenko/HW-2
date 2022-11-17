6db8926d7977f84a760dc5a68cdedae448e22554
dmytroryabenko <116278537+dmytroryabenko@users.noreply.github.com>
2022-11-17 22:17:49 +0200

    Update AverageNumbers.java

--- AverageNumbers.java
+++ AverageNumbers.java
@@ -1,19 +1,15 @@
 package Array1;
-
 public class AverageNumbers {
 
     public static void main(String[] args) {
 
         int[] array = {10, 20, 35, 18};
-
         double sum = 0;
-
+        
         for (int i : array) {
-
             sum += i;
         }
         double avg = sum / array.length;
-
         System.out.println(avg);
     }
 }
